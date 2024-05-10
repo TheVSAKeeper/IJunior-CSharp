bd0bda9a36a7d54b39e41047c241a6d30609692d
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-11 02:32:31 +0300

    Added the task "Images".

---
+++ Basics of programming/Images/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Images;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        int imagesInRow = 3;
+        int imagesInAlbum = 52;
+
+        int filledRows = imagesInAlbum / imagesInRow;
+        int unusedImages = imagesInAlbum % imagesInRow;
+
+        Console.WriteLine($"Заполненных рядов: {filledRows}\nКартинок сверх меры: {unusedImages}");
+        Console.ReadKey();
+    }
+}
