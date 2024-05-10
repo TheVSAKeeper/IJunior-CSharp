b6ca17a1492cd5289f382fab66798c2cb2743bae
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-11 01:53:30 +0300

    Added the task "Variables".

---
+++ Basics of programming/Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Variables;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        int changesCount;
+        uint millisecondsDelay;
+        byte workDaysPerWeek;
+        float interestRate;
+        bool canAddToList;
+        bool isKeepProcessing;
+        bool isWorkDay;
+        string productName;
+        string errorMessage;
+        char pluralModifier;
+    }
+}
