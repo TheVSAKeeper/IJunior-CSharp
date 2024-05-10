cb53ef8bad7b93e0e50f3125b33b3c74f7a0281c
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-11 02:06:09 +0300

    Added the task "WorkingWithStrings".

---
+++ Basics of programming/WorkingWithStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace WorkingWithStrings;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Как вас зовут?");
+        string userName = Console.ReadLine()!;
+
+        Console.WriteLine("Сколько вам лет?");
+        int userAge = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Какой ваш знак зодиака?");
+        string userZodiacSign = Console.ReadLine()!;
+
+        Console.WriteLine("Кем Вы работаете?");
+        string userProfession = Console.ReadLine()!;
+
+        Console.WriteLine($"Вас зовут {userName}, Вам {userAge}, Ваш знак зодиака {userZodiacSign}, Вы работаете {userProfession}.");
+        Console.ReadKey();
+    }
+}
