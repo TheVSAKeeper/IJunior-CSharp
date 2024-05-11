3b1cbfee0316ec5759bba7501441de388a14849e
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-11 14:36:38 +0300

    Added the task "ExitControl".

---
+++ Conditional operators and loops/ExitControl/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExitControl;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        const string ExitCommand = "exit";
+
+        string? inputMessage;
+
+        do
+        {
+            Console.WriteLine("Введите {0} для выхода из программы.", ExitCommand);
+            inputMessage = Console.ReadLine();
+        } while (inputMessage != ExitCommand);
+    }
+}
