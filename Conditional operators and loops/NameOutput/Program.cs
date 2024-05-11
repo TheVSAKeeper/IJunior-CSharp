8e30a0e063a94bbddec31334dd3343065ec5e1e9
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-11 18:10:30 +0300

    Added the task "NameOutput".

---
+++ Conditional operators and loops/NameOutput/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NameOutput;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        string defaultName = "Анонимный";
+        char defaultBorderSymbol = '%';
+
+        Console.Write($"Введите ваше имя (по умолчанию {defaultName}): ");
+        string? requiredName = Console.ReadLine();
+
+        string name = string.IsNullOrWhiteSpace(requiredName) ? defaultName : requiredName.Trim();
+
+        Console.Write($"Введите символ границы (по умолчанию {defaultBorderSymbol}): ");
+        string? requiredBorderSymbol = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(requiredBorderSymbol) || char.TryParse(requiredBorderSymbol.Trim(), out char borderSymbol) == false)
+            borderSymbol = defaultBorderSymbol;
+        
+        string nameLine = $"{borderSymbol}{name}{borderSymbol}";
+        string frame = new(borderSymbol, nameLine.Length);
+
+        string nameInFrame = $"{frame}\n"
+                             + nameLine
+                             + $"\n{frame}";
+
+        Console.WriteLine(nameInFrame);
+        Console.ReadKey();
+    }
+}
