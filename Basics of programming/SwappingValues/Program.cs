89a6a2e73782adbe9034bfa6a21a8b60c9050655
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-11 14:27:34 +0300

    Fixed indentation.

--- Basics of programming/SwappingValues/Program.cs
+++ Basics of programming/SwappingValues/Program.cs
@@ -8,9 +8,9 @@ internal static class Program
         string surname = "Иван";
 
         Console.WriteLine($"До перестановки: {name} {surname}");
-            
+
         (name, surname) = (surname, name);
-            
+
         Console.WriteLine($"После перестановки: {name} {surname}");
         Console.ReadKey();
     }
