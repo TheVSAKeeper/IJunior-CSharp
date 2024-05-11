4651795a0550fb357aef06e4c081361c5461cd5f
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-11 14:50:57 +0300

    Added the task "Sequence".

---
+++ Conditional operators and loops/Sequence/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sequence;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        int startValue = 5;
+        int endValue = 103;
+        int step = 7;
+
+        for (int i = startValue; i <= endValue; i += step)
+            Console.Write($"{i} ");
+
+        Console.ReadKey();
+    }
+}
