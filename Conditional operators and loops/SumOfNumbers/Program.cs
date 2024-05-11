dfb5865999c66a51ac1fec30107f5292e7f23de9
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-11 15:03:16 +0300

    Added the task "SumOfNumbers".

---
+++ Conditional operators and loops/SumOfNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SumOfNumbers;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Random random = new();
+        
+        int maxNumber = 99;
+        int number = random.Next(maxNumber + 1);
+
+        int firstDivisor = 3;
+        int secondDivisor = 5;
+
+        int sum = 0;
+
+        Console.Write("Числа: ");
+
+        for (int i = 0; i <= number; i++)
+        {
+            if (i % firstDivisor != 0 && i % secondDivisor != 0)
+                continue;
+
+            Console.Write($"{i} ");
+            sum += i;
+        }
+
+        Console.WriteLine($"\nСумма всех положительных чисел меньше {number} (включая число),\n"
+                          + $"которые кратные {firstDivisor} или {secondDivisor} равна {sum}.");
+    }
+}
