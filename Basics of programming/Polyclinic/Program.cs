b9f4706a4afb56ab4e9409945ee0d84c96122587
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-11 14:08:10 +0300

    Added the task "Polyclinic".

---
+++ Basics of programming/Polyclinic/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Polyclinic;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        int receptionTimeInMinutes = 10;
+        int minutesPerHour = 60;
+
+        Console.Write("Введите количество человек перед вами: ");
+        int peopleInQueue = int.Parse(Console.ReadLine());
+
+        int totalWaitingTimeInMinutes = peopleInQueue * receptionTimeInMinutes;
+        int waitingHours = totalWaitingTimeInMinutes / minutesPerHour;
+        int waitingMinutes = totalWaitingTimeInMinutes % minutesPerHour;
+
+        Console.WriteLine($"Вам нужно отстоять в очереди {waitingHours}:{waitingMinutes}.");
+        Console.ReadKey();
+    }
+}
