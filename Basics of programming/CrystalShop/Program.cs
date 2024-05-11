9b72b81e7b88625544d00802693f056eb88f43be
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-11 13:55:16 +0300

    Added the task "CrystalShop".

---
+++ Basics of programming/CrystalShop/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CrystalShop;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        int crystalPrice = 10;
+
+        Console.Write("Введите количество золота: ");
+        int.TryParse(Console.ReadLine(), out int goldInWallet);
+
+        Console.Write($"Один кристалл стоит {crystalPrice}.\n"
+                      + $"Введите количество кристаллов для покупки: ");
+
+        int.TryParse(Console.ReadLine(), out int crystalsToBuy);
+
+        int purchaseAmount = crystalsToBuy * crystalPrice;
+        goldInWallet -= purchaseAmount;
+
+        Console.WriteLine($"Кошелек\n"
+                          + $"Золото: {goldInWallet}\n"
+                          + $"Кристаллы: {crystalsToBuy}");
+
+        Console.ReadKey();
+    }
+}
