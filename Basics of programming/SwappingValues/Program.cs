0944e4ad65ff0fa4313f1c66700baa71bdfc86be
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-11 11:44:28 +0300

    Added the task "SwappingValues".

---
+++ Basics of programming/SwappingValues/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SwappingValues;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        string name = "Иванов";
+        string surname = "Иван";
+
+        Console.WriteLine($"До перестановки: {name} {surname}");
+            
+        (name, surname) = (surname, name);
+            
+        Console.WriteLine($"После перестановки: {name} {surname}");
+        Console.ReadKey();
+    }
+}
