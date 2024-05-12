44d7edc783458a550a0d53bcdbb984e608bd7878
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-12 21:08:43 +0300

    Added the task "PasswordProtectedProgram".

---
+++ Conditional operators and loops/PasswordProtectedProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PasswordProtectedProgram;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        string savedPassword = "qwe123";
+        int numberOfAttempts = 3;
+
+        for (int i = numberOfAttempts - 1; i >= 0; i--)
+        {
+            Console.Write("Введите пароль: ");
+            string enteredPassword = Console.ReadLine() ?? string.Empty;
+
+            if (savedPassword == enteredPassword)
+            {
+                Console.WriteLine("Секретное сообщение");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"Неверный пароль! Оставшиеся попытки: {i} из {numberOfAttempts}.\n");
+            }
+        }
+    }
+}
