9dc9e58654b66e2c3a16c32766b13898cb81d2da
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-12 15:07:06 +0300

    Fixed for new requirements.

--- Conditional operators and loops/CurrencyConverter/Program.cs
+++ Conditional operators and loops/CurrencyConverter/Program.cs
@@ -4,23 +4,32 @@ internal static class Program
 {
     private static void Main()
     {
+        const string RubToUsdCommand = "rub-usd";
+        const string RubToEurCommand = "rub-eur";
+        const string UsdToRubCommand = "usd-rub";
+        const string UsdToEurCommand = "usd-eur";
+        const string EurToRubCommand = "eur-rub";
+        const string EurToUsdCommand = "eur-usd";
+        
         const string Rubles = "RUB";
         const string Dollars = "USD";
         const string Euro = "EUR";
 
-        const string CurrencyNotFoundError = "Валюта {0} не найдена.";
+        const string CommandsMessage = "{0} - Конвертировать {1} в {2} (курс {3:f4})";
         const string NotEnoughCurrencyError = "На балансе недостаточно средств {0}.";
         const string AvailableForConversion = "Доступно для конвертации {0} {1}.";
         const string SumForConversionRequest = "Часть баланса для конвертации: ";
-        const string ConvertToRequest = "Перевести в ";
 
-        double dollarPriceInRubles = 98.0;
-        double euroPriceInRubles = 102.0;
-        double dollarPriceInEuros = 1.1;
+        const string BalanceMessage = $"\nРублей ({Rubles}): {{0}}\n"
+                                      + $"Долларов ({Dollars}): {{1}}\n"
+                                      + $"Евро ({Euro}): {{2}}\n\n";
+        double usdToRubRate = 98.0;
+        double eurToRubRate = 102.0;
+        double usdToEurRate = 1.1;
 
-        double rublePriceInDollars = 1 / dollarPriceInRubles;
-        double rublePriceInEuros = 1 / euroPriceInRubles;
-        double euroPriceInDollars = 1 / dollarPriceInEuros;
+        double rubToUsdRate = 1 / usdToRubRate;
+        double rubToEurRate = 1 / eurToRubRate;
+        double eurToUsdRate = 1 / usdToEurRate;
 
         double balanceInRubles = 100.1;
         double balanceInDollars = 100.2;
@@ -28,129 +37,133 @@ internal static class Program
 
         bool isWorking = true;
 
+        ConsoleKey exitKey = ConsoleKey.Q;
+
         while (isWorking)
         {
             Console.Clear();
 
-            Console.WriteLine($"Рублей ({Rubles}): {balanceInRubles}\n"
-                              + $"Долларов ({Dollars}): {balanceInDollars}\n"
-                              + $"Евро ({Euro}): {balanceInEuros}\n");
+            Console.WriteLine(BalanceMessage, balanceInRubles, balanceInDollars, balanceInEuros);
 
-            Console.Write("Конвертировать из ");
-            string fromBalance = Console.ReadLine()?.ToUpper() ?? string.Empty;
+            Console.WriteLine(CommandsMessage, RubToUsdCommand, Rubles, Dollars, rubToUsdRate);
+            Console.WriteLine(CommandsMessage, RubToEurCommand, Rubles, Euro, rubToEurRate);
+            Console.WriteLine(CommandsMessage, UsdToRubCommand, Dollars, Rubles, usdToRubRate);
+            Console.WriteLine(CommandsMessage, UsdToEurCommand, Dollars, Euro, usdToEurRate);
+            Console.WriteLine(CommandsMessage, EurToRubCommand, Euro, Rubles, eurToRubRate);
+            Console.WriteLine(CommandsMessage, EurToUsdCommand, Euro, Dollars, eurToUsdRate);
 
-            string toBalance;
+            Console.Write("\nВведите команду из списка: ");
+            string enteredCommand = Console.ReadLine()?.ToLower() ?? string.Empty;
+
             double partOfBalanceForConversion;
 
-            switch (fromBalance)
+            switch (enteredCommand)
             {
-                case Euro:
-                    Console.WriteLine(AvailableForConversion, balanceInEuros, fromBalance);
-
-                    Console.Write(SumForConversionRequest);
-                    partOfBalanceForConversion = double.Parse(Console.ReadLine() ?? string.Empty);
-
-                    if (partOfBalanceForConversion > balanceInEuros)
-                    {
-                        Console.WriteLine(NotEnoughCurrencyError, fromBalance);
-                        break;
-                    }
-
-                    Console.Write(ConvertToRequest);
-                    toBalance = Console.ReadLine()?.ToUpper() ?? string.Empty;
-
-                    if (toBalance == Dollars)
-                    {
-                        balanceInDollars += partOfBalanceForConversion * euroPriceInDollars;
-                    }
-                    else if (toBalance == Rubles)
-                    {
-                        balanceInRubles += partOfBalanceForConversion * euroPriceInRubles;
-                    }
-                    else
-                    {
-                        Console.WriteLine(CurrencyNotFoundError, fromBalance);
-                        break;
-                    }
-
-                    balanceInEuros -= partOfBalanceForConversion;
-                    break;
-
-                case Dollars:
-                    Console.WriteLine(AvailableForConversion, balanceInDollars, fromBalance);
-
-                    Console.Write(SumForConversionRequest);
-                    partOfBalanceForConversion = double.Parse(Console.ReadLine() ?? string.Empty);
-
-                    if (partOfBalanceForConversion > balanceInDollars)
-                    {
-                        Console.WriteLine(NotEnoughCurrencyError, fromBalance);
-                        break;
-                    }
-
-                    Console.Write(ConvertToRequest);
-                    toBalance = Console.ReadLine()?.ToUpper() ?? string.Empty;
-
-                    if (toBalance == Euro)
-                    {
-                        balanceInEuros += partOfBalanceForConversion * dollarPriceInEuros;
-                    }
-                    else if (toBalance == Rubles)
-                    {
-                        balanceInRubles += partOfBalanceForConversion * dollarPriceInRubles;
-                    }
-                    else
-                    {
-                        Console.WriteLine(CurrencyNotFoundError, toBalance);
-                        break;
-                    }
-
-                    balanceInDollars -= partOfBalanceForConversion;
-                    break;
-
-                case Rubles:
-                    Console.WriteLine(AvailableForConversion, balanceInRubles, fromBalance);
+                case RubToUsdCommand:
+                    Console.WriteLine(AvailableForConversion, balanceInRubles, Rubles);
 
                     Console.Write(SumForConversionRequest);
                     partOfBalanceForConversion = double.Parse(Console.ReadLine() ?? string.Empty);
 
                     if (partOfBalanceForConversion > balanceInRubles)
                     {
-                        Console.WriteLine(NotEnoughCurrencyError, fromBalance);
+                        Console.WriteLine(NotEnoughCurrencyError, Rubles);
                         break;
                     }
 
-                    Console.Write(ConvertToRequest);
-                    toBalance = Console.ReadLine()?.ToUpper() ?? string.Empty;
-
-                    if (toBalance == Dollars)
-                    {
-                        balanceInDollars += partOfBalanceForConversion * rublePriceInDollars;
-                    }
-                    else if (toBalance == Euro)
-                    {
-                        balanceInEuros += partOfBalanceForConversion * rublePriceInEuros;
-                    }
-                    else
-                    {
-                        Console.WriteLine(CurrencyNotFoundError, fromBalance);
-                        break;
-                    }
-
+                    balanceInDollars += partOfBalanceForConversion * rubToUsdRate;
                     balanceInRubles -= partOfBalanceForConversion;
                     break;
 
+                case RubToEurCommand:
+                    Console.WriteLine(AvailableForConversion, balanceInRubles, Rubles);
+
+                    Console.Write(SumForConversionRequest);
+                    partOfBalanceForConversion = double.Parse(Console.ReadLine() ?? string.Empty);
+
+                    if (partOfBalanceForConversion > balanceInRubles)
+                    {
+                        Console.WriteLine(NotEnoughCurrencyError, Rubles);
+                        break;
+                    }
+
+                    balanceInEuros += partOfBalanceForConversion * rubToEurRate;
+                    balanceInRubles -= partOfBalanceForConversion;
+                    break;
+
+                case UsdToRubCommand:
+                    Console.WriteLine(AvailableForConversion, balanceInDollars, Dollars);
+
+                    Console.Write(SumForConversionRequest);
+                    partOfBalanceForConversion = double.Parse(Console.ReadLine() ?? string.Empty);
+
+                    if (partOfBalanceForConversion > balanceInDollars)
+                    {
+                        Console.WriteLine(NotEnoughCurrencyError, Dollars);
+                        break;
+                    }
+
+                    balanceInRubles += partOfBalanceForConversion * usdToRubRate;
+                    balanceInDollars -= partOfBalanceForConversion;
+                    break;
+
+                case UsdToEurCommand:
+                    Console.WriteLine(AvailableForConversion, balanceInDollars, Dollars);
+
+                    Console.Write(SumForConversionRequest);
+                    partOfBalanceForConversion = double.Parse(Console.ReadLine() ?? string.Empty);
+
+                    if (partOfBalanceForConversion > balanceInDollars)
+                    {
+                        Console.WriteLine(NotEnoughCurrencyError, Dollars);
+                        break;
+                    }
+
+                    balanceInEuros += partOfBalanceForConversion * usdToEurRate;
+                    balanceInDollars -= partOfBalanceForConversion;
+                    break;
+
+                case EurToRubCommand:
+                    Console.WriteLine(AvailableForConversion, balanceInEuros, Euro);
+
+                    Console.Write(SumForConversionRequest);
+                    partOfBalanceForConversion = double.Parse(Console.ReadLine() ?? string.Empty);
+
+                    if (partOfBalanceForConversion > balanceInEuros)
+                    {
+                        Console.WriteLine(NotEnoughCurrencyError, Euro);
+                        break;
+                    }
+
+                    balanceInRubles += partOfBalanceForConversion * eurToRubRate;
+                    balanceInEuros -= partOfBalanceForConversion;
+                    break;
+
+                case EurToUsdCommand:
+                    Console.WriteLine(AvailableForConversion, balanceInEuros, Euro);
+
+                    Console.Write(SumForConversionRequest);
+                    partOfBalanceForConversion = double.Parse(Console.ReadLine() ?? string.Empty);
+
+                    if (partOfBalanceForConversion > balanceInEuros)
+                    {
+                        Console.WriteLine(NotEnoughCurrencyError, Euro);
+                        break;
+                    }
+
+                    balanceInDollars += partOfBalanceForConversion * eurToUsdRate;
+                    balanceInEuros -= partOfBalanceForConversion;
+                    break;
+
                 default:
-                    Console.WriteLine(NotEnoughCurrencyError, fromBalance);
+                    Console.WriteLine("Неизвестная команда");
                     break;
             }
 
-            Console.WriteLine($"Рублей: {balanceInRubles}\n"
-                              + $"Долларов: {balanceInDollars}\n"
-                              + $"Евро: {balanceInEuros}\n");
+            Console.WriteLine(BalanceMessage, balanceInRubles, balanceInDollars, balanceInEuros);
+            Console.WriteLine($"Нажмите {exitKey}, чтобы выйти, или любую клавишу, чтобы продолжить.");
 
-            Console.WriteLine("Нажмите ESC, чтобы выйти, или любую клавишу, чтобы продолжить.");
-            isWorking = Console.ReadKey().Key != ConsoleKey.Escape;
+            isWorking = Console.ReadKey().Key != exitKey;
         }
     }
 }
