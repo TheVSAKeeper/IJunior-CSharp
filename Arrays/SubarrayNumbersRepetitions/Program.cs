f5057e78037cc5c6e97dc48840f9db5b5e1eb0d8
Савелий Вотинцев <votintcev1@mail.ru>
2024-05-16 18:18:34 +0300

    Added the task "SubarrayNumbersRepetitions".

---
+++ Arrays/SubarrayNumbersRepetitions/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SubarrayNumbersRepetitions;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Random random = new();
+
+        int minValue = 0;
+        int maxValue = 2;
+
+        int size = 30;
+        int[] numbers = new int[size];
+
+        for (int i = 0; i < numbers.Length; i++)
+            numbers[i] = random.Next(minValue, maxValue + 1);
+
+        string numbersContents = string.Join(" ", numbers);
+        Console.WriteLine($"Массив: {numbersContents}");
+
+        int repeatingNumber = numbers[0];
+        int repetitionsCount = 1;
+
+        int maxRepeatingNumber = repeatingNumber;
+        int maxRepetitionsCount = repetitionsCount;
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            int number = numbers[i];
+
+            if (repeatingNumber == number)
+            {
+                repetitionsCount++;
+
+                if (repetitionsCount > maxRepetitionsCount)
+                {
+                    maxRepetitionsCount = repetitionsCount;
+                    maxRepeatingNumber = repeatingNumber;
+                }
+            }
+            else
+            {
+                repeatingNumber = number;
+                repetitionsCount = 1;
+            }
+        }
+
+        Console.WriteLine($"Число {maxRepeatingNumber} повторяется наибольшее количество раз подряд.\n"
+                          + $"Количество повторений — {maxRepetitionsCount}.");
+
+        Console.ReadKey();
+    }
+}
