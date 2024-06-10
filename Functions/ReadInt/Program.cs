2ea1fd87bf0b4fc2f89fa49732d687c06f3c4bde
Савелий Вотинцев <votintcev1@mail.ru>
2024-06-10 23:57:34 +0300

    Added the task "ReadInt".

---
+++ Functions/ReadInt/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+namespace ReadInt;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        int number = ReadInteger();
+        Console.WriteLine($"Ведено число: {number}");
+
+        number = Read<int>();
+        Console.WriteLine($"Ведено число: {number}");
+    }
+
+    private static int ReadInteger()
+    {
+        int number;
+        bool isNotNumber;
+
+        do
+        {
+            Console.Write("Введите число: ");
+            string? enteredNumber = Console.ReadLine();
+
+            isNotNumber = int.TryParse(enteredNumber, out number) == false;
+
+            if (isNotNumber)
+                Console.WriteLine($"{enteredNumber} не является числом.");
+        } while (isNotNumber);
+
+        return number;
+    }
+
+    private static T Read<T>() where T : IParsable<T>
+    {
+        string type = typeof(T).Name;
+        T? value = default;
+        bool isNotValid;
+
+        do
+        {
+            Console.Write($"Введите {type}: ");
+            string? enteredValue = Console.ReadLine();
+
+            isNotValid = T.TryParse(enteredValue, CultureInfo.InvariantCulture, out T? parsedValue) == false;
+
+            if (parsedValue != null)
+                value = parsedValue;
+
+            if (isNotValid)
+                Console.WriteLine($"{enteredValue} не является {type}.");
+        } while (isNotValid);
+
+        return value!;
+    }
+}
