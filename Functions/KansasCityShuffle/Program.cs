8d1881dc7d74128205df87eb886b093d640fd3cb
Савелий Вотинцев <votintcev1@mail.ru>
2024-06-20 13:36:00 +0300

    Renaming and simplification.
    Доработать.
    
    1 - int j - как понять по названию, что это?
    
    2 - Упростите решение, реализуйте так, как требуется в задаче

--- Functions/KansasCityShuffle/Program.cs
+++ Functions/KansasCityShuffle/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace KansasCityShuffle;
 
@@ -7,56 +6,28 @@ internal static class Program
 {
     private static void Main()
     {
-        RunShuffleTestingIntArray();
-        RunShuffleTestingStringArray();
-    }
-
-    private static void RunShuffleTestingIntArray()
-    {
-        int start = 0;
-        int[] array = CreateArray(start);
-        RunArrayTesting(array);
-    }
-
-    private static void RunShuffleTestingStringArray()
-    {
-        string repeatingElement = "test";
-        string[] array = CreateArray(repeatingElement);
-        RunArrayTesting(array);
-    }
-
-    private static void RunArrayTesting<T>(T[] array)
-    {
+        int[] array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         PrintArray(array);
         Shuffle(array);
         PrintArray(array);
     }
 
-    private static int[] CreateArray(int start, int count = 10) =>
-        Enumerable.Range(start, count)
-            .ToArray();
-
-    private static string[] CreateArray(string repeatingElement, int count = 10) =>
-        Enumerable.Repeat(repeatingElement, count)
-            .Select((element, index) => $"{element} {index}")
-            .ToArray();
-
-    private static void Shuffle<T>(T[] array)
+    private static void Shuffle(int[] array)
     {
         Random random = new();
 
         for (int i = 0; i < array.Length - 1; i++)
         {
-            int j = random.Next(i + 1, array.Length);
-            (array[i], array[j]) = (array[j], array[i]);
+            int randomIndex = random.Next(i + 1, array.Length);
+            (array[i], array[randomIndex]) = (array[randomIndex], array[i]);
         }
     }
 
-    private static void PrintArray<T>(T[] array)
+    private static void PrintArray(int[] array)
     {
-        Console.Write($"Массив {typeof(T).Name}: ");
+        Console.Write("Массив: ");
 
-        foreach (T element in array)
+        foreach (int element in array)
             Console.Write($"{element} ");
 
         Console.WriteLine();
